63b33ab7e041874e83ffafc851aa3b0111d9c5fd
Andre Almeida <aalmeida@thoughtworks.com>
2016-08-15 16:26:09 -0300

    refactoring Parser. Class

--- src/io/Parser.java
+++ src/io/Parser.java
@@ -1,7 +1,9 @@
 package io;
 
 
+import algorithm.MinimizationAlgorithm;
 import exception.FileErrorException;
+import model.Country;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -12,8 +14,12 @@ import java.util.List;
 
 public class Parser {
     private List<String> inputArray = new ArrayList<>();
-    //private final String FILE_ADDRESS = "src/resource/file.txt";
     private static final String FILE_NOT_FOUND_MESSAGE = "File not found";
+    private Country country;
+    private MinimizationAlgorithm minimizationAlgorithm;
+    private  String countryName;
+    private String productName;
+    private int productAmount;
 
 
     public List<String> readFile(String FILE_ADDRESS) throws Exception {
@@ -24,26 +30,43 @@ public class Parser {
             while (line != null) {
 
                 line = bufferedReader.readLine();
-                parseForArray(line);
+                parseLine(line);
             }
             bufferedReader.close();
 
-
-        } catch (FileNotFoundException ex) {
-                  throw new FileErrorException(FILE_NOT_FOUND_MESSAGE);
+            } catch (FileNotFoundException ex) {
+                throw new FileErrorException(FILE_NOT_FOUND_MESSAGE);
         }
 
         return inputArray;
     }
 
-    private void parseForArray(String inputLine){
+    private void parseLine(String inputLine){
+        String[] line = inputLine.split(" ");
 
-        if (inputLine != null) {
-            String[] line = inputLine.split(" ");
+        splitLine(line);
 
-            for (String value : line) {
-                inputArray.add(value);
-            }
+
+        if(line.length == 2){
+            minimizationAlgorithm.addItemToCountry(countryName, productName, productAmount);
+
+        }else if (line.length == 1) {
+            minimizationAlgorithm.addItemToRequestList(productName,productAmount);}
+    }
+
+
+    private void splitLine(String[] line){
+
+        if (line.length == 2){
+            this.countryName = line[0];
+            this.productName = line[1];
+            this.productAmount =  Integer.parseInt(line[2]);
+
+        }else if (line.length == 1){
+            this.productName = line[0];
+            this.productAmount =  Integer.parseInt(line[1]);
         }
     }
+
+
 }
